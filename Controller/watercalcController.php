00d6f82c8c04c3ffd59ff73cd058b43ac84a066c
JanisaroBRR <fredalefsr@gmail.com>
2025-07-30 09:31:06 -0300

    fix: correção nas mensagens e estruturas de erros

--- Controller/watercalcController.php
+++ Controller/watercalcController.php
@@ -6,30 +6,29 @@ use Model\WaterCalc;
 
 use Exception;
 
-class watercalcController{
+class watercalcController
+{
     private $waterModel;
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->waterModel = new WaterCalc();
     }
 
     // CALCULO E CLASSIFICAÇÃO 
-    public function calculateWeight($weight){
+    public function calculateWeight($weight)
+    {
         try {
-        
             $result = [];
-            if (isset($weight)) {
-                if ($weight > 0) {
-                    $water = round($weight *35 / (1000), 2);
-                    $result['water'] = $water;
-                } else {
-                    $result= "O peso deve conter valores positivos.";
-                }
+            if (!isset($weight) || $weight === "" || is_null($weight)) {
+                $result = "Por favor, informe o peso para obter o seu consumo diário de água.";
+            } elseif ($weight > 0) {
+                $water = round($weight * 35 / 1000, 2);
+                $result['water'] = $water;
             } else {
-                $result= "Por favor, informe o peso para obter o seu consumo diário de água.";
-            } 
+                $result = "O peso deve conter valores positivos.";
+            }
             return $result;
-
         } catch (Exception $error) {
             echo "Erro ao calcular o Consumo diário de água: " . $error->getMessage();
             return false;
@@ -38,13 +37,16 @@ class watercalcController{
 
 
     // PEGAR PESO E RESULTADO DO FRONT E ENVIAR PARA O BANCO DE DADOS
-    public function save($weight, $waterResult){
+    public function save($weight, $waterResult)
+    {
         return $this->waterModel->tabWater($weight, $waterResult);
     }
-    public function getUserWater() {
+    public function getUserWater()
+    {
         return $this->waterModel->getWater();
     }
-    public function getUserWaterLastWeek() {
+    public function getUserWaterLastWeek()
+    {
         return $this->waterModel->getWaterLastWeek();
     }
 }
--- View/diaryCalc.php
+++ View/diaryCalc.php
@@ -13,7 +13,7 @@ $userInfo = null;
 
 if (isset($_GET["id"])) {
     $user_id_from_get = $_GET["id"];
-    
+
     // Busca as informações do usuário usando o novo método getUserById
     $userInfo = $userController->getUserById($user_id_from_get);
 
@@ -53,13 +53,13 @@ $email = $userInfo["email"] ?? "";
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['weight'])) {
         $weight = $_POST['weight'];
-       
+
 
         // UTILIZANDO O CONTROLER COMO INTERMEDIÁRIO DA MANIPULAÇÃO E GERENCIAMENTO DE DADOS FRONT/BACK(BANCO DE DADOS)
         $waterResult = $watercalcController->calculateWeight($weight);
 
         // VERIFICAR SE OS CAMPOS FORAM PREENCHIDOS
-        if ($waterResult!= "O peso deve conter valores positivos." and $waterResult!="Por favor, informe o peso para obter o seu consumo diário de água.") {
+        if ($waterResult != "O peso deve conter valores positivos." and $waterResult != "Por favor, informe o peso para obter o seu consumo diário de água.") {
             $watercalcController->save($weight, $waterResult['water']);
         }
     }
@@ -70,72 +70,81 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <!DOCTYPE html>
 <html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Progresso - Hidra+</title>
-        <link rel="stylesheet" href="../templates/assets/css/diaryCalc.css">
-    </head>
-    <body>
-        <div class="dashboard-container">
-            <header class="main-header">
-                <div class="user-info">
-                    <img src="../templates/assets/img/Perfil.png" alt="Ícone do Usuário" class="user-icon">
-                    <div class="user-details">
-                        <span class="user-name"><?php echo htmlspecialchars($user_fullname); ?></span>
-                        <span class="user-email"><?php echo htmlspecialchars($email); ?></span>
-                    </div>
-                    <a href="../index.php" class="nav-button exit-button">Sair</a>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Progresso - Hidra+</title>
+    <link rel="stylesheet" href="../templates/assets/css/diaryCalc.css">
+</head>
+
+<body>
+    <div class="dashboard-container">
+        <header class="main-header">
+            <div class="user-info">
+                <img src="../templates/assets/img/Perfil.png" alt="Ícone do Usuário" class="user-icon">
+                <div class="user-details">
+                    <span class="user-name"><?php echo htmlspecialchars($user_fullname); ?></span>
+                    <span class="user-email"><?php echo htmlspecialchars($email); ?></span>
                 </div>
-                <div class="logo-container">
-                    <img src="../templates/assets/img/Logo_Hidra.png" alt="Logo Hidra+" class="logo-image">
-                </div>
-            </header>
-        </div>
-        <div class="center">
-            <div class="esquerda">
-                <div class="title-section">
-                    <h1>HIDRA+</h1>
-                    <p>Calcule a sua meta diária de consumo de água. </p>
-                    <button class="Monitoramento" id="Monitoramento">Monitoramento</button>
-                </div>
+                <a href="../index.php" class="nav-button exit-button">Sair</a>
             </div>
-            <div class="direita">
-                <form method="POST" action="diaryCalc.php">
+            <div class="logo-container">
+                <img src="../templates/assets/img/Logo_Hidra.png" alt="Logo Hidra+" class="logo-image">
+            </div>
+        </header>
+    </div>
+    <div class="center">
+        <div class="esquerda">
+            <div class="title-section">
+                <h1>HIDRA+</h1>
+                <p>Calcule a sua meta diária de consumo de água. </p>
+                <button class="Monitoramento" id="Monitoramento">Monitoramento</button>
+            </div>
+        </div>
+        <div class="direita">
+            <form method="POST" action="diaryCalc.php">
                 <div class="Caixa_Superior">
                     <h2>Cálculo</h2>
                     <p>Informe o seu peso:</p>
                     <input type="number" step="any" name="weight" placeholder="Ex: 50.3" id="userWeight">
                     <button type="submit">Calcular</button>
             </form>
-                </div>
-                <div class="Caixa_Inferior">
-                    <h2>Resultado<br><span><?php
-                    if(isset($waterResult)){
-                        echo "O consumo diário adequado de água é: ".$waterResult["water"]." litros";
-                    } else {
-                        echo "Sem resultado";
-                    }
-                    ?></span></h2>
-                </div>
-            </div>
         </div>
-        <figure class="Linha">
-            <img src="../templates/assets/img/Linha_Lateral.png" alt="Vertical decorative line with a blue gradient, used as a visual divider between sections on a clean and modern web dashboard interface" class="Linha">
-        </figure>
-        <div vw class="enabled">
-            <div vw-access-button class="active"></div>
-            <div vw-plugin-wrapper>
-                <div class="vw-plugin-top-wrapper"></div>
-            </div>
+        <div class="Caixa_Inferior">
+            <h2>Resultado<br><span><?php
+            if (isset($waterResult)) {
+                if (is_array($waterResult) && isset($waterResult["water"])) {
+                    echo "O consumo diário adequado de água é: " . $waterResult["water"] . " litros";
+                } else {
+                    echo $waterResult;
+                }
+            } else {
+                echo "Sem resultado";
+            }
+            ?></span></h2>
         </div>
-        <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
-        <script>
-            new window.VLibras.Widget('https://vlibras.gov.br/app');
-        </script>
-        <script>
-            console.log(window.innerWidth);
-        </script>
-        <script src="../templates/assets/js/diaryCalc.js"></script>
-    </body>
+    </div>
+    </div>
+    <figure class="Linha">
+        <img src="../templates/assets/img/Linha_Lateral.png"
+            alt="Vertical decorative line with a blue gradient, used as a visual divider between sections on a clean and modern web dashboard interface"
+            class="Linha">
+    </figure>
+    <div vw class="enabled">
+        <div vw-access-button class="active"></div>
+        <div vw-plugin-wrapper>
+            <div class="vw-plugin-top-wrapper"></div>
+        </div>
+    </div>
+    <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+    <script>
+        new window.VLibras.Widget('https://vlibras.gov.br/app');
+    </script>
+    <script>
+        console.log(window.innerWidth);
+    </script>
+    <script src="../templates/assets/js/diaryCalc.js"></script>
+</body>
+
 </html>
